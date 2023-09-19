cf3afbe97658ac3178a7758599f2acc9ab712b5b
Ashish Thapa <122297831+meashish11@users.noreply.github.com>
2023-09-19 10:50:18 +0545

    All Files Uploaded

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is the unit of measurement for pressure in the International System of Units (SI)?",
+        answer: "Pascal (Pa)",
+        options: [
+            "Newton (N)",
+            "Pascal (Pa)",
+            "Kilogram (kg)",
+            "Meter (m)"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which material is commonly used as a reinforcing agent in reinforced concrete?",
+        answer: "Steel",
+        options: [
+            "Aluminum",
+            "Copper",
+            "Steel",
+            "Wood"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the term for the study of the behavior of soil under load?",
+        answer: "Geotechnical Engineering",
+        options: [
+            "Structural Engineering",
+            "Geotechnical Engineering",
+            "Environmental Engineering",
+            "Transportation Engineering"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which type of bridge is supported by two or more arches under the bridge deck?",
+        answer: "Arch Bridge",
+        options: [
+            "Beam Bridge",
+            "Suspension Bridge",
+            "Cable-stayed Bridge",
+            "Arch Bridge"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the process of compacting soil to improve its engineering properties?",
+        answer: "Soil Compaction",
+        options: [
+            "Soil Erosion",
+            "Soil Stabilization",
+            "Soil Compaction",
+            "Soil Percolation"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which construction material is primarily composed of gypsum and used for interior wall finishing?",
+        answer: "Drywall (Plasterboard)",
+        options: [
+            "Concrete",
+            "Brick",
+            "Drywall (Plasterboard)",
+            "Fiberglass"
+        ]
+    },
+    {
+        numb: 7,
+        question: "In civil engineering, what does the acronym HVAC stand for?",
+        answer: "Heating, Ventilation, and Air Conditioning",
+        options: [
+            "High Voltage Alternating Current",
+            "Hydraulic Valve and Control",
+            "Heating, Ventilation, and Air Conditioning",
+            "Highway and Vehicle Control"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What type of soil has the smallest particle size and retains water most effectively?",
+        answer: "Clay",
+        options: [
+            "Silt",
+            "Gravel",
+            "Sand",
+            "Clay"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which construction method involves pouring concrete into molds or forms at the construction site?",
+        answer: "Cast-in-Place Concrete",
+        options: [
+            "Precast Concrete",
+            "Cast-in-Place Concrete",
+            "Reinforced Concrete",
+            "Shotcrete"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the primary function of a retaining wall in civil engineering?",
+        answer: "To stabilize slopes and prevent erosion",
+        options: [
+            "To provide structural support for buildings",
+            "To control temperature in buildings",
+            "To purify water",
+            "To stabilize slopes and prevent erosion"
+        ]
+    }
+];
